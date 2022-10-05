df178ff81b1d0a2fa10a0b309150a9cc67c32af6
Brian Banda <bandab45@gmail.com>
2022-10-05 13:28:30 +0200

    Update birthdaywishes.py

--- birthdaywishes.py
+++ birthdaywishes.py
@@ -1,94 +1,33 @@
-import datetime
-import time
-import requests
-import json
-#import pandas
+from datetime import datetime
+import pandas
 import random
 import smtplib
 
-year = int(datetime.datetime.now().strftime("%Y"))
-#year = 2024
-# To get year (integer input) from the user
-# year = int(input("Enter a year: "))
+MY_EMAIL_SMTP_SERVER = "YOUR EMAIL PROVIDER SMTP SERVER ADDRESS"
+MY_EMAIL = "YOUR EMAIL"
+MY_PASSWORD = "YOUR PASSWORD"
+MY_NAME = "YOUR NAME"
 
-def leap_func(year):
-    
-    # divided by 100 means century year (ending with 00)
-    # century year divided by 400 is leap year
-    if (year % 400 == 0) and (year % 100 == 0):
-        return("{0} is a leap year".format(year))
-    
-    # not divided by 100 means not a century year
-    # year divided by 4 is a leap year
-    elif (year % 4 ==0) and (year % 100 != 0):
-        return("{0} is a leap year".format(year))
-    
-    # if not divided by both 400 (century year) and 4 (not century year)
-    # year is not leap year
-    else:
-        return("{0} is not a leap year".format(year))   
-    
-#print(leap_func(year))
+today = (datetime.now().month, datetime.now().day)
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
-# Get data from realm digital api
-def get_api_data():
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+        contents = contents.replace("[MY_NAME]", MY_NAME)
 
-	r = requests.get("https://interview-assessment-1.realmdigital.co.za/do-not-send-birthday-wishes")
-
-	return r.json()
-
-api_data = get_api_data()
-
-
-my_email = "your_email@gmail.com"
-passw = "your_password"
-
-#data = pandas.read_csv("birthdays.csv")
-
-today = datetime.datetime.now()
-bday = api_data[(api_data.month == today.month) & (api_data.day == today.day)]
-name = bday["name"].tolist()
-email = bday["email"].tolist()
-status = bday["status"].tolist()
-
-employees = []
-
-for n in range(len(name)):
-    employees.append(
-        {
-            "name": name[n],
-            "email": email[n]
-            "status": status[n]
-        }
-    )
-
-if not in employees:
-    print("no birthdays today")
-    # stops the current iteration and continues with the next one
-    continue
-elif  employees["status"] == "No Longer Employee":
-    print("Employee no longer works for Realm Digital")
-    continue
-elif  employees["status"] == "Not Yet Employee":
-    print("Is not yet a Realm Digital Employee")
-    continue 
-elif  employees["status"] == "No Birthday Wishes":
-    print("Is not supposed to receive birthday wishes")
-    continue    
-else:
-    for employee in employees:
-        num = random.randint(1, 3)
-        with open(f"letterTemplates/letter_{num}.txt") as letter:
-            lines = letter.readlines()
-            lines[0].strip()
-            lines[0] = lines[0].replace("[NAME]", employee["name"])
-            message = "".join(lines)
-
-        with smtplib.SMTP("smtp.gmail.com") as connection:
-            connection.starttls()
-            connection.login(user=my_email, password=passw)
-            connection.sendmail(from_addr=my_email, to_addrs=employee["email"], msg=f"Subject: HAPPY BIRTHDAY\n\n{message}")
-            print(f"message sent to {employee['name']}" + leap_func(year))
+    with smtplib.SMTP(MY_EMAIL_SMTP_SERVER) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
             
             
                        
